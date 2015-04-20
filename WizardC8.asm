99622bfb592a11ee02fc840a147ee5b52935e7c5
Ephraim Becker <emb16@outlook.com>
2015-04-19 21:13:34 -0400

    Update WizardC8.asm

--- WizardC8.asm
+++ WizardC8.asm
@@ -20,10 +20,10 @@ Init:
 	.dw Icon			;or .dw $0000 if you don't have an icon
 	.dw $0000				;usually .dw $0000 if you don't have or know what an ALE is
 Start:                          	;main routines
-    call OpenGUIStack
+    OpenGUIStack()
     
     ld hl, WizardC8Window
-    call GUIRLargeWindow
+    call GUIRLargeWin
 	
 	
 	
